0a12814b4ee369d1cfe69f48f8c4954edfbb77cc
Caleb Pomar <cpsharksfan@gmail.com>
2017-09-28 08:37:03 -0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	int dropSingle = 1;
+	int dropMultiple = 2;
+	int showOptions = 3;
+	int quit = 4;
+	int userInput = 0;
+	int slotNumber = 0;
+	int pathDirection = 0;
+	int numChips = 0;
+	double positionChip = 0.0;
+	double totalWinning = 0.0;
+	double averageWinnings = 0.0;
+
+	double winningsPos0 = 100;
+	double winningsPos1 = 500;
+	double winningsPos2 = 1000;
+	double winningsPos3 = 0;
+	double winningsPos4 = 10000;
+	double winningsPos5 = 0;
+	double winningsPos6 = 1000;
+	double winningsPos7 = 500;
+	double winningsPos8 = 100;
+
+	srand(42);
+
+	cout << "Welcome to the Plinko simulator!  Enter 3 to see options." << endl << endl;
+	cout << "Enter your selection now:" << endl;
+
+
+
+}
